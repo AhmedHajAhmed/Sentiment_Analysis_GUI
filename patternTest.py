33d5c583d5e7012017eb4d022545dbf15e4f4fb9
Ahmed Haj Ahmed <92589844+AhmedHajAhmed@users.noreply.github.com>
2022-12-25 12:31:06 -0500

    Delete patternTest.py

--- patternTest.py
+++
@@ -1,9 +0,0 @@
-from pattern.text.en import sentiment
-
-thing = "I dislike coffee"
-
-print(sentiment(thing))
-for word in thing.split():
-    print(f"{word}: {sentiment(word)}")
-
-print(sentiment("very grey"))
